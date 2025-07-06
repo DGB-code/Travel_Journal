54213adad89b0101087b2f9dd260133a512e08b4
DGBcode <asyrowmaksat123@gmail.com>
2025-07-06 09:08:09 +0000

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Globe_icon.svg" alt="globe icon" />
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
